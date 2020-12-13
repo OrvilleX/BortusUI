3c38d1ad17209d9078e5ad0f7cc1876829888c3e
姚正发 <976691141@qq.com>
2020-12-13 14:03:37 +0800

    完成store开发

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,21 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { IApiState } from "./modules/api";
+import { IAppState } from "./modules/app";
+import { IPermissonState } from "./modules/permission";
+import { ISettingsState } from "./modules/settings";
+import { ITagsViewState } from "./modules/tagsView";
 import { IUserState } from './modules/user';
 
 Vue.use(Vuex);
 
 export interface IRootState {
   user: IUserState
+  app: IAppState
+  tagsView: ITagsViewState
+  api: IApiState
+  permission: IPermissonState
+  settings: ISettingsState
 }
 
 export default new Vuex.Store<IRootState>({});
