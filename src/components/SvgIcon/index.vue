40b8d5ed7a78b91ea30fbd1a4a3f1c174305b660
姚正发 <976691141@qq.com>
2020-12-19 23:55:31 +0800

    完成svgicon开发

--- src/components/SvgIcon/index.vue
+++ src/components/SvgIcon/index.vue
@@ -1,46 +1,47 @@
 <template>
-  <div v-if="isExternal" :style="styleExternalIcon" class="svg-external-icon svg-icon" v-on="$listeners" />
+  <div
+    v-if="isExternal"
+    :style="styleExternalIcon"
+    class="svg-external-icon svg-icon"
+    v-on="$listeners"
+  />
   <svg v-else :class="svgClass" aria-hidden="true" v-on="$listeners">
     <use :href="iconName" />
   </svg>
 </template>
 
-<script>
-// doc: https://panjiachen.github.io/vue-element-admin-site/feature/component/svg-icon.html#usage
-import { isExternal } from '@/utils/validate'
+<script lang="ts">
+import { Component, Vue, Prop } from "vue-property-decorator";
+import { isExternal } from "@/utils/validate";
 
-export default {
-  name: 'SvgIcon',
-  props: {
-    iconClass: {
-      type: String,
-      required: true
-    },
-    className: {
-      type: String,
-      default: ''
+@Component({
+  name: "SvgIcon",
+})
+export default class extends Vue {
+  @Prop({ required: true }) iconClass!: string;
+  @Prop({ default: "" }) className!: string;
+
+  get isExternal() {
+    return isExternal(this.iconClass);
+  }
+
+  get iconName() {
+    return `#icon-${this.iconClass}`;
+  }
+
+  get svgClass() {
+    if (this.className) {
+      return "svg-icon " + this.className;
+    } else {
+      return "svg-icon";
     }
-  },
-  computed: {
-    isExternal() {
-      return isExternal(this.iconClass)
-    },
-    iconName() {
-      return `#icon-${this.iconClass}`
-    },
-    svgClass() {
-      if (this.className) {
-        return 'svg-icon ' + this.className
-      } else {
-        return 'svg-icon'
-      }
-    },
-    styleExternalIcon() {
-      return {
-        mask: `url(${this.iconClass}) no-repeat 50% 50%`,
-        '-webkit-mask': `url(${this.iconClass}) no-repeat 50% 50%`
-      }
-    }
+  }
+
+  get styleExternalIcon() {
+    return {
+      mask: `url(${this.iconClass}) no-repeat 50% 50%`,
+      "-webkit-mask": `url(${this.iconClass}) no-repeat 50% 50%`,
+    };
   }
 }
 </script>
@@ -56,7 +57,7 @@ export default {
 
 .svg-external-icon {
   background-color: currentColor;
-  mask-size: cover!important;
+  mask-size: cover !important;
   display: inline-block;
 }
 </style>
